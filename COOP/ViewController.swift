97266b4a87819ec7a96453220447327d89389ea6
VIJVERMAN Kamiel (s) <kamiel.vijverman@dt-c205-03.local>
2017-05-18 09:28:22 +0200

    Initial Commit

---
+++ COOP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  COOP
+//
+//  Created by VIJVERMAN Kamiel (s) on 18/05/2017.
+//  Copyright © 2017 VIJVERMAN Kamiel (s). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
