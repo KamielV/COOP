f0f84102bf010607ca3ea09421520f4dc8fc5c06
VIJVERMAN Kamiel (s) <kamiel.vijverman@dt-c205-03.local>
2017-05-22 14:30:19 +0200

    foto aangepast

--- COOP/FotoViewController.swift
+++ COOP/FotoViewController.swift
@@ -5,15 +5,29 @@
 //  Created by VIJVERMAN Kamiel (s) on 22/05/2017.
 //  Copyright © 2017 VIJVERMAN Kamiel (s). All rights reserved.
 //
+//foto tutorial http://www.oodlestechnologies.com/blogs/Open-Image-Gallery-and-Take-photo-in-Swift
 
 import UIKit
 
-class FotoViewController: UIViewController {
-
+class FotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var imagePicker:UIImagePickerController? = UIImagePickerController()
+    @IBOutlet weak var imgFoto: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        imagePicker?.delegate = self
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            imagePicker!.allowsEditing = false
+            imagePicker!.sourceType = UIImagePickerControllerSourceType.camera
+            imagePicker!.cameraCaptureMode = .photo
+            present(imagePicker!, animated: true, completion: nil)
+        } else {
+            let alert = UIAlertController(title: "Camera Not Found", message: "This device has no Camera", preferredStyle: .alert)
+            let ok = UIAlertAction(title: "OK", style:.default, handler: nil)
+            alert.addAction(ok)
+            present(alert, animated: true, completion: nil)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +35,15 @@ class FotoViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
-    */
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imgFoto.contentMode = .scaleAspectFit
+        imgFoto.image = chosenImage
+        dismiss(animated: true, completion: nil)
+    }
 
 }
