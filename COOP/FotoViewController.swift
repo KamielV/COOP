fe4ccb1ef819462fbc1d06f8eea68973ea74017a
VIJVERMAN Kamiel (s) <kamiel.vijverman@dt-c205-03.local>
2017-05-23 09:23:29 +0200

    foto doorgestuurd

--- COOP/FotoViewController.swift
+++ COOP/FotoViewController.swift
@@ -43,8 +43,88 @@ class FotoViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage
         imgFoto.contentMode = .scaleAspectFit
         imgFoto.image = chosenImage
-        var imgData = UIImagePNGRepresentation(chosenImage)
         dismiss(animated: true, completion: nil)
+    }
+    // verstuur data van http://www.kaleidosblog.com/how-to-upload-images-using-swift-2-send-multipart-post-request
+    @IBAction func btnVerstuurData(_ sender: Any) {
+        let url = URL(string: "sitemerlinhier")
+        
+        let request = NSMutableURLRequest(url: url!)
+        request.httpMethod = "POST"
+        
+        let boundary = generateBoundaryString()
+        
+        
+        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+        
+        if (imgFoto.image == nil)
+        {
+            print("no foto found")
+            return
+        }
+        
+        let image_data = UIImageJPEGRepresentation(imgFoto.image!, 1.0)
+        
+        
+        if(image_data == nil)
+        {
+            print("no data found")
+            return
+        }
+        
+        
+        let body = NSMutableData()
+        
+        let filename = "groepfoto.jpg"
+        let mimetype = "image/jpg"
+        
+        
+        body.append("--\(boundary)\r\n".data(using: String.Encoding.utf8)!)
+        body.append("Content-Disposition:form-data; name=\"foto\"\r\n\r\n".data(using: String.Encoding.utf8)!)
+        body.append("hi\r\n".data(using: String.Encoding.utf8)!)
+        
+        
+        
+        body.append("--\(boundary)\r\n".data(using: String.Encoding.utf8)!)
+        body.append("Content-Disposition:form-data; name=\"file\"; filename=\"\(filename)\"\r\n".data(using: String.Encoding.utf8)!)
+        body.append("Content-Type: \(mimetype)\r\n\r\n".data(using: String.Encoding.utf8)!)
+        body.append(image_data!)
+        body.append("\r\n".data(using: String.Encoding.utf8)!)
+        
+        
+        body.append("--\(boundary)--\r\n".data(using: String.Encoding.utf8)!)
+        
+        
+        request.httpBody = body as Data
+        
+        
+        
+        let session = URLSession.shared
+        
+        
+        let task = session.dataTask(with: request as URLRequest, completionHandler: {
+            (
+            data, response, error) in
+            
+            guard ((data) != nil), let _:URLResponse = response, error == nil else {
+                print("error")
+                return
+            }
+            
+            if let dataString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+            {
+                print(dataString)
+            }
+            
+        }) 
+        
+        task.resume()
+        
+    }
+    
+    func generateBoundaryString() -> String
+    {
+        return "Boundary-\(UUID().uuidString)"
     }
 
 }
